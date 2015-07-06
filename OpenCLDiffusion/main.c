e695f2f6e8393e8d415f3a01666bcaec99eed0d1
scrblnrd3 <amalan.iyengar@gmail.com>
2015-07-06 10:11:42 -0400

    Initial Commit

---
+++ OpenCLDiffusion/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  OpenCLDiffusion
+//
+//  Created by Amalan Iyengar on 7/6/15.
+//  Copyright (c) 2015 Amalan Iyengar. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
